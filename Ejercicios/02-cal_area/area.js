0ca1f15a7f71e5f4351a9d118ce39e6b31a8a737
Mesach Venegas <62820347+Azteck-Dev@users.noreply.github.com>
2020-05-04 19:15:00 -0700

    ejercicio 1 actualizado

--- Ejercicios/02-cal_area/area.js
+++ Ejercicios/02-cal_area/area.js
@@ -5,39 +5,67 @@ Escribe un programa que pueda calcular el area de 3 figuras geometricas, triangu
     circulo = pi * r2(pi por radio al cuadrado)
 */ 
 
-let figura = prompt('Figuras disponibles,Triaungulo, Rectangulo y Circulo.    De que figura quiere calcular el area:');
+let figura = prompt('Introdusca la figura de la que quiere clacular el area: Traingulo, Rectangulo o Circulo.');
 
-if(figura == 'triangulo' || figura == 'Triangulo'){
-    let base = prompt('Introduzca la base: ');
-    let altura = prompt('Introduzca la altura: ');
-    // formula b*h/2.
-    let area = base*altura/2;
+let base;
+let altura;
+let radio;
 
-    console.log(`El area del Triangulo con base ${base} cm2 y altura ${altura} cm2 es: ${area}cm2.`)
+switch(figura){
+    case 'triangulo':
+        base = prompt('Ingrese la base del triangulo: ');
+        altura = prompt('Ingrese la altura del triangulo: ');
+        console.log(`El area del triangulo es: ${(base*altura)/2}cm2.`);
+        break;
+    case 'rectangulo':
+        base = prompt('Ingrese la base del rectangulo:');
+        altura = prompt('Ingrese la altura del rectangulo: ');
+        console.log(`El area del rectangulo es: ${base*altura}cm2`);
+        break;
+    case 'circulo':
+        // la funcion Math.pow me permite usar un exponente en un numero
+        radio = prompt('Ingrese el radio del circulo: ');
+        console.log(`El area del circulo es: ${Math.PI*Math.pow(radio,2)}`);
+        break;
+    default: console.log('Por favor introduzca alguna de las figuras validas.');
 }
-else if(figura == 'rectangulo' || figura == 'Rectangulo'){
-    let base = prompt('Introduzca la base: ');
-    let altura = prompt('Introduzca la altura: ');
-    // formula b*h.
-    let area = base*altura;
 
-    console.log(`El area de Rectangulo con base ${base} cm2 y altura ${altura} cm2 es de ${area} cm2.`)
-}
-else if(figura == 'circulo' || figura == 'Circulo'){
-    let radio = prompt('Introduzca el radio del circulo: ');
-    let radio2 = radio * radio; 
-    let pi = Math.PI;
-    // formula pi*radio^2.
-    let area = pi * radio2;
 
-    console.log(`El area del circulo con un radio de ${radio} es de ${area}.`)
+/*  ----------------------- Esta fue mi solucion para el ejercicio ----------------------- */ 
+
+// let figura = prompt('Figuras disponibles,Triaungulo, Rectangulo y Circulo.    De que figura quiere calcular el area:');
+
+// if(figura == 'triangulo' || figura == 'Triangulo'){
+//     let base = prompt('Introduzca la base: ');
+//     let altura = prompt('Introduzca la altura: ');
+//     // formula b*h/2.
+//     let area = base*altura/2;
+
+//     console.log(`El area del Triangulo con base ${base} cm2 y altura ${altura} cm2 es: ${area}cm2.`)
+// }
+// else if(figura == 'rectangulo' || figura == 'Rectangulo'){
+//     let base = prompt('Introduzca la base: ');
+//     let altura = prompt('Introduzca la altura: ');
+//     // formula b*h.
+//     let area = base*altura;
+
+//     console.log(`El area de Rectangulo con base ${base} cm2 y altura ${altura} cm2 es de ${area} cm2.`)
+// }
+// else if(figura == 'circulo' || figura == 'Circulo'){
+//     let radio = prompt('Introduzca el radio del circulo: ');
+//     let radio2 = radio * radio; 
+//     let pi = Math.PI;
+//     // formula pi*radio^2.
+//     let area = pi * radio2;
+
+//     console.log(`El area del circulo con un radio de ${radio} es de ${area}.`)
 
     
-}
-else{
-    console.log(`"${figura}" no es una de las opciones disponibles por favor introduzca una figura valida de las siguientes.`);
+// }
+// else{
+//     console.log(`"${figura}" no es una de las opciones disponibles por favor introduzca una figura valida de las siguientes.`);
 
-    console.log('Triangulo');
-    console.log('Rectangulo');
-    console.log('Circulo');
-}
+//     console.log('Triangulo');
+//     console.log('Rectangulo');
+//     console.log('Circulo');
+// }
