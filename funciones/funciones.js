82e5c4cd01f965325befc6be8fb74554d1357a3e
Mesach Venegas <62820347+Azteck-Dev@users.noreply.github.com>
2020-05-04 17:29:05 -0700

    Funciones

---
+++ funciones/funciones.js
@@ -0,0 +1,23 @@
+
+    // function saludar(){
+    //     console.log("Hola soy una funcion :)");
+    // }
+
+    // saludar();
+    // saludar();
+
+    // const saludar = () => console.log("Hola soy una funcion :)");
+    // saludar();
+
+    // const saludarUsuario = (user) => {
+    //     console.log(`Hola ${user}, soy una funcion con parametros.`);
+    // }
+
+    // saludarUsuario('Mesach');
+
+    const suma = (num1,num2) => num1+num2;
+
+    let resultado = suma(2,7);
+
+    console.log(resultado);
+    
