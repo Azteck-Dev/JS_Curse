1a15e5fcc1e7b6edcf50dbb9790e79a13c5f7aa2
Mesach Venegas <62820347+Azteck-Dev@users.noreply.github.com>
2020-05-04 19:26:09 -0700

    Ejercicio 3 solucion

--- Ejercicios/03-par_impar/par_impar.js
+++ Ejercicios/03-par_impar/par_impar.js
@@ -10,11 +10,13 @@ Solicita un numero e imprime todos los numeros pares e impares desde 1 hasta ese
     Formula x/2 = 0 es numero par
 */ 
 
+let num = prompt('Introduce un Numero:');
 
-let num = prompt('Introduce un numero: ');
-
-for(let i=1;i<=num;i++){
-    
-       (num % 2 != 0) ? console.log(`${i} es impar`) : console.log(`${i} es par`);
+for(let i=1; i<=num; i++){
+    if(i%2 == 0){
+        console.log(`El numero ${i} es par`);
+    }
+    else{
+        console.log(`El numero ${i} es impar`);
+    }
 }
-
