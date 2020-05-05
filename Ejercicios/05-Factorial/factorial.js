b3766490d4619b93fb16dc2cb961486d8995780e
Mesach Venegas <62820347+Azteck-Dev@users.noreply.github.com>
2020-05-04 20:10:07 -0700

    Solucion Ejercicio 5

--- Ejercicios/05-Factorial/factorial.js
+++ Ejercicios/05-Factorial/factorial.js
@@ -1,5 +1,13 @@
 /*
 Escriba un programa que pida un numero mayor entero que 0 y calcule su factorial. El factorial es el resultado de multiplicar ese numero por sus anteriores hasta la unidad.
-
         !5 = (5)(4)(3)(2)(1) = 120
 */
+
+let num = prompt('Ingrese un numero:');
+let resultado = 1;
+
+for(let i=num;i>0;i--){
+        resultado *= i;
+}
+
+console.log(`El factorial de ${num} es ${resultado}`);
