ccb265e533fcb5b773a93865e4af10fe14f7eaef
Mesach Venegas <62820347+Azteck-Dev@users.noreply.github.com>
2020-05-04 19:49:34 -0700

    Ejercicio 4 resuelto

--- Ejercicios/04-Num_primo/primo.js
+++ Ejercicios/04-Num_primo/primo.js
@@ -2,7 +2,22 @@
 Escribir un programa que pida un numero entero mayor que 1 y que escriba si es un numero primo o no(Un numero primo es aquel que solo es divisible por si mismo y la unidad).
 */ 
 
+let num = prompt('Introduzca un numero:');
+let divisor = 0;
 
-let num = prompt('Introduzca un numero.');
+if(num == 1){
+    console.log('El numero no es valido.')
+}
+else{
+    for(let i=2; i<num; i++){
+        if(num % i == 0){
+            console.log(`El numero ${num} no es primo`);
+            divisor++
+            break;
+        }   
+    }
+}
 
-let primos = [2,3,5,7,11];
+if(divisor == 0){
+    console.log(`El numero ${num} es primo`);
+}
