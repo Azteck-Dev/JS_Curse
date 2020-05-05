965004ef8d87c2676049c6d1e1f9aad37b1a81ba
Mesach Venegas <62820347+Azteck-Dev@users.noreply.github.com>
2020-05-04 21:16:30 -0700

    Solucion ejercicios 6 y 7

--- Ejercicios/06-Suma_max/suma.js
+++ Ejercicios/06-Suma_max/suma.js
@@ -1,3 +1,14 @@
 /*
  Escribe un programa que permite ir introduciendo una serie indeterminada de números mientras su suma no supera 50. Cuando esto ocurre, se debe mostrar el total acumulado y el contador de cuantos números se han introducido.
 */ 
+
+let suma = 0;
+let count = 0;
+
+while(suma <= 50){
+    suma += parseInt(prompt('Ingrese un numero para incluir en la suma.'));
+    count++
+}
+
+console.log(`La suma total es de: ${suma}`);
+console.log(`El total de numeros introducidos es: ${count}`);
--- Ejercicios/07-Arrays/arrays.js
+++ Ejercicios/07-Arrays/arrays.js
@@ -1,3 +1,24 @@
 /*
-Crea 3 matrices. El primero tendra 5 números y el segundo se llamará pares y el tercero impares, ambos afectados vacíos. Después de multiplicar cada uno de los números del primer conjunto por un número aleatorio entre 1 y 10, si el resultado es por guarda ese número en el conjunto de pares y si es impar en el conjunto de impares. Muestra por consola: -la multiplicación que se produce junto con su resultado con el formato 2 x 3 = 6 -el conjunto de pares e impares
+Crea 3 matrices. El primero tendra 5 números y el segundo se llamará pares y el tercero impares, ambos afectados vacíos. Después de multiplicar cada uno de los números del primer conjunto por un número aleatorio entre 1 y 10, si el resultado es par, guardar ese número en el conjunto de pares y si es impar en el conjunto de impares. Muestra por consola: -la multiplicación que se produce junto con su resultado con el formato 2 x 3 = 6 -el conjunto de pares e impares
 */
+
+const numeros = [1,7,9,12,23];
+let pares = [];
+let impares = [];
+
+for(const numero of numeros){
+    const random = Math.round(Math.random() * 10 + 1);    
+    let resultado = numero * random;
+
+    console.log(`${numero} x ${random} = ${numero * random}`);
+
+    if(resultado % 2 == 0){
+        pares.push(resultado);
+    }
+    else{
+        impares.push(resultado);
+    }
+}
+
+console.log(`De los cuales los siguientes son pares: ${pares.join(', ')}.`);
+console.log(`De los cuales los siguientes son impares: ${impares.join(', ')}.`);
